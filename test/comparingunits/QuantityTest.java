7c714a97aa82e08d428469c2881ba3ffaf81b314
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-03-22 12:52:06 +0530

    Refactor Tests

--- test/comparingunits/QuantityTest.java
+++ test/comparingunits/QuantityTest.java
@@ -8,21 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityTest {
     @Test
-    void compareOneFeetWithTwelveInches() {
+    void compareOneFeetWithEquivalentInches() {
         Quantity oneFeet = new Quantity(Unit.FEET, new BigDecimal(1));
         Quantity twelveFeet = new Quantity(Unit.INCH, new BigDecimal(12));
         assertEquals(oneFeet, twelveFeet);
     }
 
     @Test
-    void compareTwoInchesWithFiveCentimetre() {
+    void compareTwoInchesWithEquivalentInches() {
         Quantity fiveCentimeter = new Quantity(Unit.CENTIMETER, new BigDecimal(5));
         Quantity twoInch = new Quantity(Unit.INCH, new BigDecimal(2));
         assertEquals(fiveCentimeter, twoInch);
     }
 
     @Test
-    void compareOneCentimeterWithTenMillimeter() {
+    void compareOneCentimeterWithEquivalentMillimeters() {
         Quantity oneCentimeter = new Quantity(Unit.CENTIMETER, new BigDecimal(1));
         Quantity tenMillimeter = new Quantity(Unit.MILLIMETER, new BigDecimal(10));
         assertEquals(oneCentimeter, tenMillimeter);
@@ -36,28 +36,29 @@ class QuantityTest {
     }
 
     @Test
-    void shouldNotCompareOneGallonWithOneMillimeter() {
+    void shouldNotCompareDifferentUnits() {
         Quantity oneGallon = new Quantity(Unit.GALLON, new BigDecimal(1));
         Quantity oneMillimeter = new Quantity(Unit.MILLIMETER, new BigDecimal(3.78));
+        System.out.println();
         assertNotEquals(oneGallon, oneMillimeter);
     }
 
     @Test
-    void addShouldAddIfGivenQuantitiesHaveSameUnit() throws IncompatibleTypeException {
+    void shouldAddQuantitiesWithSameUnits() throws IncompatibleTypeException {
         Quantity oneInch = new Quantity(Unit.INCH, new BigDecimal(1));
         Quantity twoInch = new Quantity(Unit.INCH, new BigDecimal(2));
         assertEquals(twoInch, oneInch.add(oneInch));
     }
 
     @Test
-    void addShouldThrowExceptionIfGivenQuantitiesHaveDifferentUnit() {
+    void addShouldThrowExceptionforQuantitiesOfDifferentUnit() {
         Quantity oneInch = new Quantity(Unit.INCH, new BigDecimal(1));
         Quantity oneGallon = new Quantity(Unit.GALLON, new BigDecimal(1));
         assertThrows(IncompatibleTypeException.class, () -> oneInch.add(oneGallon));
     }
 
     @Test
-    void shouldAddTwoInchAndTwoPointFiveCentimeterAndReturnInInches() throws IncompatibleTypeException {
+    void shouldAddValuesInDifferentUnitsAndReturnSumInInches() throws IncompatibleTypeException {
         Quantity twoInch = new Quantity(Unit.INCH, new BigDecimal(2));
         Quantity twoPointFiveCentimeter = new Quantity(Unit.CENTIMETER, new BigDecimal(2.5));
         Quantity threeInch = new Quantity(Unit.INCH, new BigDecimal(3));
